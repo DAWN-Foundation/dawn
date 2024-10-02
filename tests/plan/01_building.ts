7ed0eeae7263dd5dd01b193896e9fcab175bd88c
Tommy Hash <thxsh@pm.me>
2024-10-02 16:25:46 +0300

    Test cannot add duplicate buildings

--- tests/plan/01_building.ts
+++ tests/plan/01_building.ts
@@ -1,8 +1,8 @@
 import * as anchor from '@coral-xyz/anchor'
-import { Program, BN } from '@coral-xyz/anchor'
+import { Program, BN, AnchorError } from '@coral-xyz/anchor'
 import NodeWallet from '@coral-xyz/anchor/dist/cjs/nodewallet'
 import { assert } from 'chai'
-import { Keypair, PublicKey } from '@solana/web3.js'
+import { Keypair, PublicKey, SendTransactionError } from '@solana/web3.js'
 
 import { Plan } from '../../target/types/plan'
 import { mock } from './utils'
@@ -40,5 +40,27 @@ describe('plan::building', () => {
     assert.equal(building.address, address)
     assert.equal(building.floors, floors)
     assert.equal(building.bump, buildingBump)
+  })
+
+  it('cannot add the building with the same name and address', async () => {
+    const name = 'Building 1'
+    const address = '123 Main St'
+    const floors = 5
+
+    try {
+      await program.methods
+        .addBuilding(name, address, floors)
+        .accounts({ caller: mock.buildingOwner.publicKey })
+        .signers([mock.buildingOwner])
+        .rpc()
+      assert.ok(false)
+    } catch (error) {
+      assert.ok(error instanceof SendTransactionError)
+      const err: SendTransactionError = error
+      assert.strictEqual(
+        err.transactionError.message,
+        'Transaction simulation failed: Error processing Instruction 0: custom program error: 0x0',
+      )
+    }
   })
 })
