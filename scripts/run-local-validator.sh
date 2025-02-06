22b21fa44ea999d8b94f4fd0ab6504bc3b618996
Tommy Hash <thxsh@pm.me>
2025-02-06 15:48:58 +0200

    Add bind address to local solana validator command

--- scripts/run-local-validator.sh
+++ scripts/run-local-validator.sh
@@ -4,4 +4,5 @@ solana-test-validator \
   --reset \
   --bpf-program CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C raydium/raydium.so \
   --account DNXgeM9EiiaAbaWvwjHj9fQQLAX5ZsfHyvmYUNRAdNC8 raydium/pool_fee_receiver.json \
-  --account D4FPEruKEHrG5TenZ2mpDGEfu1iUvTiqBxvpU8HLBvC2 raydium/raydium_config.json
+  --account D4FPEruKEHrG5TenZ2mpDGEfu1iUvTiqBxvpU8HLBvC2 raydium/raydium_config.json \
+  --bind-address 0.0.0.0
