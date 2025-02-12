108e99d3c332ab00b34e49bdd02ec05892dd25fd
Tommy Hash <thxsh@pm.me>
2025-02-12 10:45:21 +0200

    6 month fix

--- tests/dawn/05_plan.ts
+++ tests/dawn/05_plan.ts
@@ -518,7 +518,7 @@ export const planTests = () =>
     test('adds a plan with a start time 6 months in the future', async () => {
       const sixMonthsLater = new Date()
       sixMonthsLater.setMonth(sixMonthsLater.getMonth() + 5)
-      sixMonthsLater.setDate(sixMonthsLater.getDate() + 30)
+      sixMonthsLater.setDate(sixMonthsLater.getDate() + 29)
       const startAt = new BN(sixMonthsLater.getTime() / 1000)
 
       const speed = 600 // to have new PDA
