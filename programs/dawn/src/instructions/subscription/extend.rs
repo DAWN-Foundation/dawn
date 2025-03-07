326e9d1a89cbb272dd6ef3d5df6f13e2a9761225
Tommy Hash <thxsh@pm.me>
2025-03-07 15:10:32 +0200

    Test extend subscription

--- programs/dawn/src/instructions/subscription/extend.rs
+++ programs/dawn/src/instructions/subscription/extend.rs
@@ -1,7 +1,16 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
+use raydium_cp_swap::{program::RaydiumCpSwap, states::PoolState};
+use solana_program::clock::SECONDS_PER_DAY;
 
 use super::{Config, DawnApp, Plan, Subscription};
-use crate::{events::SubscriptionExtended, utils::optional_pubkey_seed};
+use crate::{
+    error::DawnError, events::SubscriptionExtended, instructions::PaymentAccounts,
+    utils::optional_pubkey_seed,
+};
 
 #[derive(Accounts)]
 pub struct ExtendSubscription<'info> {
@@ -45,15 +54,150 @@ pub struct ExtendSubscription<'info> {
         bump = subscription.bump
     )]
     pub subscription: Box<Account<'info, Subscription>>,
+
+    // MINTS
+    /// The DAWN mint account
+    #[account(address = config.dawn_mint)]
+    pub dawn_mint: Box<Account<'info, Mint>>,
+    /// The USDC mint account
+    #[account(address = config.usdc_mint)]
+    pub usdc_mint: Box<Account<'info, Mint>>,
+
+    // RAYDIUM
+    /// The Raydium program account
+    #[account(address = config.raydium)]
+    pub raydium: Program<'info, RaydiumCpSwap>,
+
+    /// The Raydium authority account
+    /// CHECK: Verified against the config
+    #[account(address = config.raydium_authority)]
+    pub raydium_authority: UncheckedAccount<'info>,
+
+    /// The Raydium config account
+    /// CHECK: Verified in Raydium upon CPI
+    #[account(address = config.raydium_config)]
+    pub raydium_config: UncheckedAccount<'info>,
+
+    /// The Raydium pool account
+    /// CHECK: Address checked to match the pool from config
+    #[account(mut, address = config.raydium_pool)]
+    pub raydium_pool: AccountLoader<'info, PoolState>,
+
+    /// The Raydium observation account
+    /// CHECK: Address checked to match the pool observation key
+    #[account(mut, address = config.raydium_observation)]
+    pub raydium_observation: UncheckedAccount<'info>,
+
+    // VAULTS
+    /// The DAWN pool vault account
+    #[account(
+        mut,
+        token::mint = dawn_mint,
+    )]
+    pub raydium_dawn_vault: Box<Account<'info, TokenAccount>>,
+
+    /// The USDC pool vault account
+    #[account(
+        mut,
+        token::mint = usdc_mint,
+    )]
+    pub raydium_usdc_vault: Box<Account<'info, TokenAccount>>,
+
+    // TOKEN ACCOUNTS
+    /// The callers associated DAWN token account
+    #[account(
+        mut,
+        associated_token::mint = dawn_mint,
+        associated_token::authority = caller,
+    )]
+    pub user_dawn_account: Box<Account<'info, TokenAccount>>,
+
+    /// The callers associated USDC token account
+    #[account(
+        mut,
+        associated_token::mint = usdc_mint,
+        associated_token::authority = caller,
+    )]
+    pub user_usdc_account: Box<Account<'info, TokenAccount>>,
+
+    /// The DAWN DAO DAWN token account
+    #[account(mut, address = config.dao_dawn_account)]
+    pub dao_dawn_account: Box<Account<'info, TokenAccount>>,
+
+    /// The Validator DAWN pool token account
+    #[account(mut,address = config.validator_dawn_account)]
+    pub validator_dawn_account: Box<Account<'info, TokenAccount>>,
+
+    /// The Medallion DAWN pool token account
+    #[account(mut, address = config.medallion_dawn_account)]
+    pub medallion_dawn_account: Box<Account<'info, TokenAccount>>,
+
+    /// The device owner escrow USDC token vault
+    #[account(
+        init_if_needed,
+        payer = caller,
+        associated_token::mint = usdc_mint,
+        associated_token::authority = plan,
+    )]
+    pub escrow_usdc_vault: Box<Account<'info, TokenAccount>>,
+
+    /// The device owner escrow DAWN token vault
+    #[account(
+        init_if_needed,
+        payer = caller,
+        associated_token::mint = dawn_mint,
+        associated_token::authority = plan,
+    )]
+    pub escrow_dawn_vault: Box<Account<'info, TokenAccount>>,
+
+    // PROGRAMS
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
 }
 
 impl DawnApp {
     pub fn extend_subscription(ctx: Context<ExtendSubscription>) -> Result<()> {
+        let config = &ctx.accounts.config;
         let plan = &ctx.accounts.plan;
+
+        // TODO: Make sure cannot extend if already expired
+
+        let payment_accounts = PaymentAccounts {
+            caller: &ctx.accounts.caller,
+            raydium_pool: &ctx.accounts.raydium_pool,
+            raydium_authority: &ctx.accounts.raydium_authority,
+            raydium_config: &ctx.accounts.raydium_config,
+            raydium_observation: &ctx.accounts.raydium_observation,
+            usdc_mint: &ctx.accounts.usdc_mint,
+            dawn_mint: &ctx.accounts.dawn_mint,
+            raydium: &ctx.accounts.raydium,
+            raydium_usdc_vault: &ctx.accounts.raydium_usdc_vault,
+            raydium_dawn_vault: &ctx.accounts.raydium_dawn_vault,
+            user_usdc_account: &ctx.accounts.user_usdc_account,
+            user_dawn_account: &ctx.accounts.user_dawn_account,
+            dao_dawn_account: &ctx.accounts.dao_dawn_account,
+            validator_dawn_account: &ctx.accounts.validator_dawn_account,
+            medallion_dawn_account: &ctx.accounts.medallion_dawn_account,
+            escrow_usdc_vault: &ctx.accounts.escrow_usdc_vault,
+            escrow_dawn_vault: &ctx.accounts.escrow_dawn_vault,
+            token_program: &ctx.accounts.token_program,
+        };
+
+        let (_, _, swap_price) = Self::process_payment(payment_accounts, config, plan)?;
+
         let subscription = &mut ctx.accounts.subscription;
 
-        // Calculate subscription expiration by adding plan `duration` days to current subscription expiration
-        let expiration = subscription.expiration + (plan.duration as i64);
+        // Calculate plan duration in seconds (days to seconds)
+        let duration_in_seconds = (plan.duration as u64)
+            .checked_mul(SECONDS_PER_DAY)
+            .ok_or(DawnError::Overflow)?;
+
+        // Calculate subscription expiration by adding plan `duration` days to existing expiration
+        let expiration = subscription
+            .expiration
+            .checked_add(duration_in_seconds as i64)
+            .ok_or(DawnError::Overflow)?;
 
         // Save subscription data
         subscription.expiration = expiration;
@@ -64,6 +208,7 @@ impl DawnApp {
             subscriber: ctx.accounts.caller.key(),
             device: subscription.device,
             expiration: subscription.expiration,
+            swap_price,
             created_at: subscription.created_at,
         });
 
--- tests/dawn/07_subscription.ts
+++ tests/dawn/07_subscription.ts
@@ -52,6 +52,7 @@ interface SubscriptionExtended {
   subscriber: PublicKey
   device: PublicKey | null
   expiration: BN
+  swapPrice: BN
   createdAt: number
 }
 
@@ -462,55 +463,179 @@ export const subscriptionTests = () =>
       assert.equal(subscription.bump, mock.subscriptionBump)
     })
 
-    test('extend subscribe', async () => {
-      let subscription = await program.account.subscription.fetch(
+    test('extends subscription', async () => {
+      await new Promise((resolve) => setTimeout(resolve, 300))
+
+      // get expiration before extension
+      const subscriptionBefore = await program.account.subscription.fetch(
         mock.subscriptionPda,
       )
+      const expirationBefore = new BN(subscriptionBefore.expiration)
 
-      const plan = await program.account.plan.fetch(mock.planPda)
-
-      const extendedExpiration = subscription.expiration.add(
-        new BN(plan.duration),
+      const testerUsdcBalanceBefore = await getBalance(
+        provider.connection,
+        mock.customerUsdcAccount,
       )
+
+      const daoDawnBalanceBefore = await getBalance(
+        provider.connection,
+        mock.daoDawnAccount,
+      )
+
+      const validatorDawnBalanceBefore = await getBalance(
+        provider.connection,
+        mock.validatorDawnAccount,
+      )
+
+      const medallionDawnBalanceBefore = await getBalance(
+        provider.connection,
+        mock.medallionDawnAccount,
+      )
+
+      const escrowUsdcBalanceBefore = await getBalance(
+        provider.connection,
+        accounts.escrowUsdcVault,
+      )
+
+      const escrowDawnBalanceBefore = await getBalance(
+        provider.connection,
+        accounts.escrowDawnVault,
+      )
+
+      // get balances of raydium vaults
+      const raydiumDawnVault = await getAccount(
+        provider.connection,
+        accounts.raydiumDawnVault,
+      )
+      const raydiumUsdcVault = await getAccount(
+        provider.connection,
+        accounts.raydiumUsdcVault,
+      )
+
+      const dawnVaultAmount = new BN(raydiumDawnVault.amount.toString())
+      const usdcVaultAmount = new BN(raydiumUsdcVault.amount.toString())
+      const price = dawnVaultAmount.mul(Q32).div(usdcVaultAmount)
 
       const tx = await program.methods
         .extendSubscription()
-        .accounts({
-          caller: mock.customer.publicKey,
-          config: mock.configPda,
-          plan: mock.planPda,
-          subscription: mock.subscriptionPda,
-        })
+        .accounts(accounts)
         .signers([mock.customer])
         .transaction()
 
       const txDetails = await confirmTx(provider, tx)
+
+      // make sure event was emitted
       const event = await getEvent<SubscriptionExtended>(
         program,
         txDetails,
         'SubscriptionExtended',
       )
-
-      // make sure the subscription event was generated with new extend subscription
       assert.ok(event.subscription.equals(mock.subscriptionPda))
       assert.ok(event.plan.equals(mock.planPda))
       assert.ok(event.subscriber.equals(mock.customer.publicKey))
       expect(event.device).toBeNull()
+      assert.ok(
+        event.expiration.eq(
+          expirationBefore.add(new BN(plan.duration * SECONDS_PER_DAY)),
+        ),
+      )
+      assert.ok(event.swapPrice.gt(price))
       expect(new BN(event.createdAt).gt(new BN(0))).toBeTruthy()
-      assert.ok(event.expiration.cmp(extendedExpiration) === 0)
 
-      // make sure the subscription was extended
-      await new Promise((resolve) => setTimeout(resolve, 2000))
-      const extendedSubscription = await program.account.subscription.fetch(
-        mock.subscriptionPda,
+      // make sure the customer USDC account was debited
+      const testerUsdcBalanceAfter = await getBalance(
+        provider.connection,
+        mock.customerUsdcAccount,
+      )
+      assert.ok(testerUsdcBalanceAfter.lt(testerUsdcBalanceBefore))
+
+      // make sure the amount debited is the plan price with 0.25% tolerance (to account for slippage)
+      const diff = testerUsdcBalanceBefore.sub(testerUsdcBalanceAfter)
+      assert.ok(diff.gte(plan.price.mul(TOLERANCE_BPS).div(BPS_DENOMINATOR)))
+
+      // calculate total fee in USDC
+      const totalFeeBps = mock.daoFee
+        .add(mock.validatorFee)
+        .add(mock.medallionFee)
+      const totalUsdcFee = plan.price.mul(totalFeeBps).div(BPS_DENOMINATOR)
+
+      // make sure the device owner escrow USDC vault account was debited
+      const usdcRemainder = plan.price.sub(totalUsdcFee)
+      const dailyUsdc = usdcRemainder.div(new BN(plan.duration))
+      const dailyDawn = dailyUsdc.mul(price).div(Q32)
+      const usdcExpected = usdcRemainder.sub(dailyUsdc)
+      const escrowUsdcBalanceAfter = await getBalance(
+        provider.connection,
+        accounts.escrowUsdcVault,
+      )
+      assert.ok(
+        escrowUsdcBalanceAfter.sub(escrowUsdcBalanceBefore).eq(usdcExpected),
       )
 
-      expect(new BN(extendedSubscription.createdAt).gt(new BN(0))).toBeTruthy()
-      assert.ok(extendedSubscription.plan.equals(mock.planPda))
-      assert.ok(extendedSubscription.subscriber.equals(mock.customer.publicKey))
-      expect(extendedSubscription.device).toBeNull()
-      assert.equal(extendedSubscription.bump, mock.subscriptionBump)
-      assert.ok(extendedSubscription.expiration.cmp(extendedExpiration) === 0)
+      // make sure the device owner escrow DAWN vault account was credited with the daily DAWN portion
+      const escrowDawnBalanceAfter = await getBalance(
+        provider.connection,
+        accounts.escrowDawnVault,
+      )
+      assert.ok(
+        escrowDawnBalanceAfter.sub(escrowDawnBalanceBefore).eq(dailyDawn),
+      )
+
+      const totalDawn = totalUsdcFee.add(dailyUsdc).mul(price).div(Q32)
+      const totalDawnWithSlippage = totalDawn
+        .mul(BPS_DENOMINATOR.sub(new BN(100)))
+        .div(BPS_DENOMINATOR)
+      const totalDawnFee = totalDawnWithSlippage.sub(dailyDawn)
+
+      // make sure the DAO DAWN account was credited with the DAO fee portion
+      const daoFee = totalDawnFee.mul(mock.daoFee).div(totalFeeBps)
+      const daoDawnBalanceAfter = await getBalance(
+        provider.connection,
+        mock.daoDawnAccount,
+      )
+      assert.ok(daoDawnBalanceAfter.sub(daoDawnBalanceBefore).eq(daoFee))
+
+      // make sure the validator DAWN account was credited with the validator fee portion
+      const validatorFee = totalDawnFee.mul(mock.validatorFee).div(totalFeeBps)
+      const validatorDawnBalanceAfter = await getBalance(
+        provider.connection,
+        mock.validatorDawnAccount,
+      )
+      assert.ok(
+        validatorDawnBalanceAfter
+          .sub(validatorDawnBalanceBefore)
+          .eq(validatorFee),
+      )
+
+      // make sure the medallion DAWN account was credited with the medallion fee portion
+      const medallionFee = totalDawnFee.mul(mock.medallionFee).div(totalFeeBps)
+      const medallionDawnBalanceAfter = await getBalance(
+        provider.connection,
+        mock.medallionDawnAccount,
+      )
+      assert.ok(
+        medallionDawnBalanceAfter
+          .sub(medallionDawnBalanceBefore)
+          .gte(medallionFee),
+      )
+
+      // TODO >> DAWN-121: Refactor
+      // get block time, and calculate expected expiration
+      // let expiration = txDetails.blockTime + plan.duration * SECONDS_PER_DAY
+
+      // make sure the subscription was created
+      let subscription = await program.account.subscription.fetch(
+        mock.subscriptionPda,
+      )
+      expect(new BN(subscription.createdAt).gt(new BN(0))).toBeTruthy()
+      assert.ok(subscription.plan.equals(mock.planPda))
+      assert.ok(subscription.subscriber.equals(mock.customer.publicKey))
+      expect(subscription.device).toBeNull()
+      // assert.equal(subscription.expiration.toNumber(), expiration)
+      // assert.ok(subscription.lastClaim.eq(new BN(txDetails.blockTime)))
+      assert.ok(subscription.claimableDawn.gte(dailyDawn))
+      assert.ok(subscription.dailyUsdc.eq(dailyUsdc))
+      assert.equal(subscription.bump, mock.subscriptionBump)
     })
 
     test('cannot subscribe to the same plan twice', async () => {
