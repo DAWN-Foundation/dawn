0f722ef1dedf0d327388d7d2454e61d4a9a93788
Vitalii Koval <vitalii.koval.dev@gmail.com>
2025-07-29 14:17:47 +0400

    uncomment client export

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,5 +1,5 @@
 // SDK Barrel Exports
-// export * from './client' // TODO: Enable after TypeScript config fixes
+export * from './client' // TODO: Enable after TypeScript config fixes
 export * from './pda'
 export * from './utils'
 export * from './integrations'
