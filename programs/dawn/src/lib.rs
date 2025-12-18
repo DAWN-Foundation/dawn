fc4ed39863003f55934a63f272f0b211b735145c
root <root@vmi2814531.contaboserver.net>
2025-12-18 16:44:33 +0100

    undo local program id

--- programs/dawn/src/lib.rs
+++ programs/dawn/src/lib.rs
@@ -4,7 +4,7 @@
 use anchor_lang::prelude::*;
 
 #[cfg(not(feature = "devnet"))]
-declare_id!("5AUcCAfGvpPsDcqGaESHFGg3JbeDLmvhntxYXncPQq8p");
+declare_id!("F4Yq1jQgccrzbEn9iHrA9JjQ1xyRFViDX8Xhb5FzJmaE");
 
 #[cfg(feature = "devnet")]
 declare_id!("dawnC74ugJiaQsLgRUfaTiWDmJpNq9cXo3E1NgdqwjP");
